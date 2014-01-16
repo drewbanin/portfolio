a981cb66dd5dad7b0c27f8871f1f6d349cd2a934
Drew Banin <dsb@drexel.edu>
2014-01-16 00:41:21 -0500

    Fixed schema db name problemz

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,6 @@ ActiveRecord::Schema.define(version: 20140116052025) do
     t.datetime "updated_at"
   end
 
-  add_index "metrics", ["metric_type_id"], name: "index_metrics_on_metric_type_id"
+  add_index "metrics", ["metric_type_id"], name: "index_metrics_on_metric_type_id", using: :btree
 
 end
